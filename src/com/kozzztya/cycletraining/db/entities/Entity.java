9364f62bcc1a2ad9fc245c42ae73f394889bd0ea
kostyan <kozzztya@gmail.com>
2014-07-16 21:27:54 +0300

    Added serialization for passing data by intent

--- src/com/kozzztya/cycletraining/db/entities/Entity.java
+++ src/com/kozzztya/cycletraining/db/entities/Entity.java
@@ -1,6 +1,9 @@
 package com.kozzztya.cycletraining.db.entities;
 
-public interface Entity {
+import java.io.Serializable;
+
+public interface Entity extends Serializable {
     public long getId();
+
     public void setId(long id);
 }
