c235c5f13ec1e49e1b0648e5abcc1c19ec9e9488
kostyan <kozzztya@gmail.com>
2014-07-26 15:20:21 +0300

    Added statistic module

--- src/com/kozzztya/cycletraining/statistic/StatisticCreateActivity.java
+++ src/com/kozzztya/cycletraining/statistic/StatisticCreateActivity.java
@@ -1,25 +1,71 @@
 package com.kozzztya.cycletraining.statistic;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import com.kozzztya.cycletraining.DrawerActivity;
 import com.kozzztya.cycletraining.R;
+import com.kozzztya.cycletraining.db.DBHelper;
+import com.kozzztya.cycletraining.db.datasources.ExercisesDS;
+import com.kozzztya.cycletraining.db.datasources.TrainingJournalDS;
 
 
-public class StatisticCreateActivity extends Activity {
+public class StatisticCreateActivity extends DrawerActivity {
+
+    private Spinner spinnerExercises;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.statistic_create);
+        super.onCreate(savedInstanceState, R.layout.statistic_create);
 
+        spinnerExercises = (Spinner) findViewById(R.id.spinnerExercise);
+        setTitle(getString(R.string.statistic));
+    }
+
+    @Override
+    protected void onStart() {
         fillExerciseSpinner();
+        super.onStart();
     }
 
     private void fillExerciseSpinner() {
+        //Select exercises that used in training journal
+        SQLiteDatabase db = DBHelper.getInstance(this).getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT e._id, " + ExercisesDS.COLUMN_NAME + " from " + ExercisesDS.TABLE_NAME +
+                " e, " + TrainingJournalDS.TABLE_NAME + " tj \n" +
+                "WHERE tj." + TrainingJournalDS.COLUMN_EXERCISE + " = e._id GROUP BY " + ExercisesDS.COLUMN_NAME, null);
 
+        String[] adapterCols = new String[]{ExercisesDS.COLUMN_NAME, "_id"};
+        int[] adapterRowViews = new int[]{android.R.id.text1};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item,
+                cursor, adapterCols, adapterRowViews, 0);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerExercises.setAdapter(adapter);
     }
 
     public void confirmClick(View view) {
+        Cursor cursor = (Cursor) spinnerExercises.getSelectedItem();
 
+        if (cursor != null) {
+            Spinner spinnerValue = (Spinner) findViewById(R.id.spinnerValue);
+            Spinner spinnerСriterion = (Spinner) findViewById(R.id.spinnerСriterion);
+            Spinner spinnerPeriod = (Spinner) findViewById(R.id.spinnerPeriod);
+
+            long exerciseId = cursor.getLong(cursor.getColumnIndex("_id"));
+            String resultFunc = (String) spinnerValue.getSelectedItem();
+            String values = (String) spinnerСriterion.getSelectedItem();
+            String period = (String) spinnerPeriod.getSelectedItem();
+
+            Intent intent = new Intent(this, StatisticShowActivity.class);
+            intent.putExtra("exerciseId", exerciseId);
+            intent.putExtra("resultFunc", resultFunc);
+            intent.putExtra("values", values);
+            intent.putExtra("period", period);
+            startActivity(intent);
+        }
     }
 }
--- src/com/kozzztya/cycletraining/statistic/StatisticShowActivity.java
+++ src/com/kozzztya/cycletraining/statistic/StatisticShowActivity.java
@@ -1,12 +1,88 @@
 package com.kozzztya.cycletraining.statistic;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+import com.kozzztya.cycletraining.MyActionBarActivity;
+import com.kozzztya.cycletraining.R;
 
+import java.util.Calendar;
 
-public class StatisticShowActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+public class StatisticShowActivity extends MyActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistic_show);
+        if (!handleChartData()) {
+            finish();
+            Toast.makeText(this, getString(R.string.toast_chart_error), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean handleChartData() {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            long exerciseId = extras.getLong("exerciseId");
+            String resultFunc = extras.getString("resultFunc");
+            String values = extras.getString("values");
+            String period = extras.getString("period");
+
+            getSupportActionBar().setTitle(values);
+            getSupportActionBar().setSubtitle(resultFunc + " " + getString(R.string.result));
+
+            //Find out sql function
+            if (resultFunc.equals(getString(R.string.result_avg))) {
+                resultFunc = "avg";
+            } else if (resultFunc.equals(getString(R.string.result_max))) {
+                resultFunc = "max";
+            } else {
+                return false;
+            }
+
+            //Find out begin date of chosen period
+            long minPeriod;
+            if (period.equals(getString(R.string.period_all))) {
+                minPeriod = 0;
+            } else if (period.equals(getString(R.string.period_year))) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.YEAR, -1);
+                minPeriod = calendar.getTimeInMillis();
+            } else if (period.equals(getString(R.string.period_half_year))) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.MONTH, -6);
+                minPeriod = calendar.getTimeInMillis();
+            } else if (period.equals(getString(R.string.period_three_months))) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.MONTH, -3);
+                minPeriod = calendar.getTimeInMillis();
+            } else if (period.equals(getString(R.string.period_month))) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.MONTH, -1);
+                minPeriod = calendar.getTimeInMillis();
+            } else {
+                return false;
+            }
+
+            //Find out chart values
+            AbstractChart chart;
+            if (values.equals(getString(R.string.weight_reps_date))) {
+                chart = new WeightRepsDateChart(this);
+            } else if (values.equals(getString(R.string.weight_date))) {
+                chart = new WeightDateChart(this);
+            } else if (values.equals(getString(R.string.reps_weight))) {
+                chart = new RepsWeightChart(this);
+            } else {
+                return false;
+            }
+
+            View chartView = chart.buildChartView(exerciseId, resultFunc, minPeriod);
+            if (chartView != null) {
+                FrameLayout layout = (FrameLayout) findViewById(R.id.chart);
+                layout.addView(chartView);
+                return true;
+            }
+        }
+        return false;
     }
 }
