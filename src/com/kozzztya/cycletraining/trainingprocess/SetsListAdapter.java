8187562d44b3d41f8d733667ea54663d767d1986
kostyan <kozzztya@gmail.com>
2014-09-06 11:07:47 +0300

    Use ViewHolder pattern

--- src/com/kozzztya/cycletraining/trainingprocess/SetsListAdapter.java
+++ src/com/kozzztya/cycletraining/trainingprocess/SetsListAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
 import com.kozzztya.cycletraining.R;
 import com.kozzztya.cycletraining.db.entities.Set;
 import com.kozzztya.cycletraining.utils.SetUtils;
@@ -20,32 +21,42 @@ public class SetsListAdapter extends ArrayAdapter<Set> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ViewHolder viewHolder;
+        View view = convertView;
+
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
             view = inflater.inflate(R.layout.set_list_item, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.textViewSetN = (TextView) view.findViewById(R.id.textViewSetN);
+            viewHolder.textViewReps = (TextView) view.findViewById(R.id.textViewReps);
+            viewHolder.textViewWeight = (TextView) view.findViewById(R.id.textViewWeight);
+            viewHolder.textViewComment = (TextView) view.findViewById(R.id.textViewComment);
+
+            view.setTag(viewHolder);
         } else {
-            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
         }
 
         Set set = getItem(position);
 
-        TextView textViewSetN = (TextView) view.findViewById(R.id.textViewSetN);
-        TextView textViewReps = (TextView) view.findViewById(R.id.textViewReps);
-        TextView textViewWeight = (TextView) view.findViewById(R.id.textViewWeight);
-
-        textViewSetN.setText(String.valueOf(position + 1));
-        textViewReps.setText(set.getReps());
-        textViewWeight.setText(SetUtils.weightFormat(set.getWeight()));
+        viewHolder.textViewSetN.setText(String.valueOf(position + 1));
+        viewHolder.textViewReps.setText(set.getReps());
+        viewHolder.textViewWeight.setText(SetUtils.weightFormat(set.getWeight()));
 
         String comment = set.getComment();
         if (comment != null && comment.length() != 0) {
-            TextView textViewComment = (TextView) view.findViewById(R.id.textViewComment);
-            textViewComment.setText(comment);
-            textViewComment.setVisibility(View.VISIBLE);
+            viewHolder.textViewComment.setText(comment);
+            viewHolder.textViewComment.setVisibility(View.VISIBLE);
         }
-
         return view;
     }
 
+    static class ViewHolder {
+        TextView textViewSetN;
+        TextView textViewReps;
+        TextView textViewWeight;
+        TextView textViewComment;
+    }
 }
